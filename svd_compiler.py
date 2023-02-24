c7a5015feb44e594648e16cc39f095b59f2446f5
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2023-02-24 14:03:53 -0600

    setup interface for cli

--- svd_compiler.py
+++ svd_compiler.py
@@ -1,4 +1,10 @@
+import argparse
 import xml.etree.ElementTree as ET
+from argparse import RawTextHelpFormatter
+import readline
+
+descText = "Compile SVD per peripheral to one file describing entire MCU"
+parser = argparse.ArgumentParser(description=descText, formatter_class=RawTextHelpFormatter)
 
 svd_output_name = 'svd_me14.svd' 
 
